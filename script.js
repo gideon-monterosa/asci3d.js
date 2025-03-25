9607298ffd249a46c199962af84c312a55768d9d
Gideon Monterosa <gideon.monterosa@gmail.com>
2025-03-25 17:32:34 +0100

    fill the terminal with random ascii chars

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
 const ascii = document.querySelector("#ascii");
 
-ascii.innerText = "Test";
+const asciiChars = [" ", ".", "-", "+", "*", "#", "%", "@"];
+
+const width = 100;
+const height = 35;
+
+let asciiText = "";
+
+for (let y = 0; y < height; y++) {
+  for (let x = 0; x < width; x++) {
+    const randomIndex = Math.floor(Math.random() * asciiChars.length);
+    asciiText += asciiChars[randomIndex];
+  }
+  asciiText += "\n";
+}
+
+ascii.innerText = asciiText;
