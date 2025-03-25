b391208cb3d61b548a0e0a605323b9011ba92e5e
Gideon Monterosa <gideon.monterosa@gmail.com>
2025-03-25 20:09:08 +0100

    add a function to draw squares

--- script.js
+++ script.js
@@ -1,7 +1,5 @@
 const ascii = document.querySelector("#ascii");
 
-// const asciiChars = [" ", ".", "-", "+", "*", "#", "%", "@"];
-
 const width = 100;
 const height = 35;
 
@@ -32,6 +30,10 @@ function renderBuffer() {
  * @property {number} y
  */
 
+/**
+ * @param {number} x
+ * @param {number} y
+ */
 function createPoint2d(x, y) {
   return { x, y };
 }
@@ -40,32 +42,50 @@ function createPoint2d(x, y) {
  * @param {Point2D} p1
  * @param {Point2D} p2
  */
-function drawLine(p1, p2) {
-  const dx = Math.abs(p2.x - p1.x);
-  const dy = Math.abs(p2.y - p1.y);
-  const sx = p1.x < p2.x ? 1 : -1;
-  const sy = p1.y < p2.y ? 1 : -1;
+function drawLine(p1, p2, char = "#") {
+  x0 = p1.x;
+  x1 = p2.x;
+  y0 = p1.y;
+  y1 = p2.y;
+  const dx = Math.abs(x1 - x0);
+  const dy = Math.abs(y1 - y0);
+  const sx = x0 < x1 ? 1 : -1;
+  const sy = y0 < y1 ? 1 : -1;
   let err = dx - dy;
 
   while (true) {
-    if (p1.x >= 0 && p1.x < width && p1.y >= 0 && p1.y < height) {
-      buffer[p1.y][p1.x] = "#";
+    if (x0 >= 0 && x0 < width && y0 >= 0 && y0 < height) {
+      buffer[y0][x0] = char;
     }
-    if (p1.x === p2.x && p1.y === p2.y) break;
+    if (x0 === x1 && y0 === y1) break;
     const e2 = 2 * err;
     if (e2 > -dy) {
       err -= dy;
-      p1.x += sx;
+      x0 += sx;
     }
     if (e2 < dx) {
       err += dx;
-      p1.y += sy;
+      y0 += sy;
     }
   }
 }
 
+/**
+ * @param {Point2D} p1
+ * @param {Point2D} p3
+ */
+function drawRectangle(p1, p3) {
+  const p2 = createPoint2d(p1.x, p3.y);
+  const p4 = createPoint2d(p3.x, p1.y);
+
+  drawLine(p1, p2);
+  drawLine(p2, p3);
+  drawLine(p3, p4);
+  drawLine(p4, p1);
+}
+
 initBuffer();
 
-drawLine(createPoint2d(3, 3), createPoint2d(9, 9));
+drawRectangle(createPoint2d(3, 3), createPoint2d(9, 9));
 
 renderBuffer();
